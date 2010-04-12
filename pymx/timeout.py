a9d8869bb18ee52581154949080b6d4d8b6e3dfa
Piotr Findeisen <piotr.findeisen@gmail.com>
2010-04-13 00:03:51 +0200

    total timeout counter

---
+++ pymx/timeout.py
@@ -0,0 +1,49 @@
+
+from time import time
+
+
+class Timeout(object):
+    """Makes calculating remaining time before timeout easy.
+
+    Example use:
+
+    ..python:
+        q = Queue()
+        timer = Timeout(5)
+        while timer.remaining:
+            x = q.get(timeout=timer.timeout)
+            if accept(x):
+                return x
+    """
+
+    def __init__(self, timeout):
+        """Creates new reusable, total-limiting timer. If `timeout` is
+        ``None``, the timer created has no limits. """
+        if timeout is None:
+            self._time_end = None
+        else:
+            self._time_end = time() + timeout
+
+        self._undefined = object()
+        self._next_timeout = self._undefined
+
+    @property
+    def remaining(self):
+        """Returns value convertible to ``True`` iff there is some time
+        remaining. Also caches next value of `timeout` property, so it safe to
+        call ``wait(timeout=timer.timeout)`` after ``timer.remaining`` returned
+        ``True``-like value. """
+        if self._time_end is None:
+            self._next_timeout = None
+            return True
+        else:
+            self._next_timeout = max(self._time_end - time(), 0)
+            return self._next_timeout
+
+    @property
+    def timeout(self):
+        """Returns time remaining or None if no time limit was specified. """
+        if self._next_timeout is self._undefined:
+            remaining = self.remaining # force recalcualtion of _next_timeout
+        next, self._next_timeout = self._next_timeout, self._undefined
+        return next
---
+++ test/test_timeout.py
@@ -0,0 +1,32 @@
+
+from time import time
+from Queue import Queue, Empty
+
+from nose.tools import assert_almost_equal, timed
+
+from pymx.timeout import Timeout
+
+def test_timeout():
+    timer = Timeout(None)
+    assert timer.remaining
+    assert timer.timeout is None
+    assert timer.timeout is None
+
+    timer = Timeout(0.1)
+    assert timer.remaining
+    assert_almost_equal(timer.timeout, 0.1, 3)
+
+@timed(0.5)
+def test_timeout_partition():
+    q = Queue()
+    timer = Timeout(0.1)
+    i = 0
+    while timer.remaining:
+        try:
+            q.get(timeout=min(timer.timeout, 0.01))
+        except Empty:
+            pass
+        i += 1
+
+    assert_almost_equal(i, 10, -1)
+
