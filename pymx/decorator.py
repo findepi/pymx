6b63c5452ad56e4839916caf538f15e8d162f1be
Piotr Findeisen <piotr.findeisen@gmail.com>
2010-04-14 15:40:54 +0200

    parametrizable_decorator: support positional arguments

--- pymx/decorator.py
+++ pymx/decorator.py
@@ -10,6 +10,9 @@ def _update_wrapper(wrapper, wrapped, *args, **kwargs):
 def parametrizable_decorator(decorator):
     @wraps(decorator)
     def wrapper(fn=None, *args, **kwargs):
+        if fn is not None and not callable(fn):
+            args = (fn,) + args
+            fn = None
         if fn is not None:
             return _update_wrapper(decorator(fn, *args, **kwargs), fn)
         else:
